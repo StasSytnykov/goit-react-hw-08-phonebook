7f4b32e0e796ac2bf4034819ca7e449402316faf
Stas Sytnykov <stas82kak05@gmail.com>
2022-06-30 12:37:58 +0300

    fixed base path on app

--- src/components/UserMenu/UserMeny.jsx
+++ src/components/UserMenu/UserMeny.jsx
@@ -5,12 +5,12 @@ import { getUserEmail } from 'redux/auth/authSelectors';
 import { logout } from 'redux/auth/authOperations';
 
 export const UserMenu = () => {
-  // const userEmail = useSelector(getUserEmail);
+  const userEmail = useSelector(getUserEmail);
   const dispatch = useDispatch();
 
   return (
     <>
-      {/* <Typography sx={{ mr: 2 }}>{userEmail ? userEmail : null}</Typography> */}
+      <Typography sx={{ mr: 2 }}>{userEmail ? userEmail : null}</Typography>
       <Button onClick={() => dispatch(logout())} color="inherit">
         Logout
       </Button>
