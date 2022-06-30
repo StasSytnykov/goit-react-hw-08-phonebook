9f065440e3566db8fc3ccbdb8157ca9811a5f8c2
Stas Sytnykov <stas82kak05@gmail.com>
2022-06-30 12:48:59 +0300

    try to fix home view

--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,10 @@ export const App = () => {
     <Suspense fallback={<div>Loading...</div>}>
       <Routes>
         <Route path="*" element={<Navigate to="/register" />} />
+        <Route
+          path="/goit-react-hw-08-phonebook"
+          element={<Navigate to="/register" />}
+        />
         <Route path="/" element={<AppBarView />}>
           <Route path="/contacts" element={<ContactsView />} />
           <Route path="/register" element={<RegisterView restricted />} />
